b30d4d8178e5427166617a188acadaf69631832b
enteragoodusername <ninjasword1999@gmail.com>
2025-03-14 09:45:53 -0400

    j

--- init.lua
+++ init.lua
@@ -57,7 +57,32 @@ require("mason-lspconfig").setup_handlers {
 	end
 }
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-require('lspconfig').gdscript.setup(capabilities)
+local lspconfig = require('lspconfig')
+lspconfig.gdscript.setup(capabilities)
+
+
+
+lspconfig.rust_analyzer.setup({
+    on_attach = on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+})
 vim.diagnostic.config({
   virtual_text = true,
 })
