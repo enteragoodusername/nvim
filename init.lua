ac5d7873d5b3ccb418eede17f9efef8be28a9e98
john <ninjasword1999@gmail.com>
2025-04-06 20:12:48 +0000

    Added transparency config

--- init.lua
+++ init.lua
@@ -86,7 +86,24 @@ lspconfig.rust_analyzer.setup({
 vim.diagnostic.config({
   virtual_text = true,
 })
-
+ -- Optional, you don't have to run setup.
+require("transparent").setup({
+  -- table: default groups
+  groups = {
+    'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+    'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+    'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+    'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
+    'EndOfBuffer',
+  },
+  -- table: additional groups that should be cleared
+  extra_groups = {},
+  -- table: groups you don't want to clear
+  exclude_groups = {},
+  -- function: code to be executed after highlight groups are cleared
+  -- Also the user event "TransparentClear" will be triggered
+  on_clear = function() end,
+})
 require'cmp'.setup {
   sources = {
     { name = 'nvim_lsp' }
